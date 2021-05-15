cc62eee8afff43b72d8d621e8e711e4806702eb9
Gabriel Porangaba <g.limap97@gmail.com>
2021-05-14 22:16:45 -0300

    Correcção de variavel NOME

--- src/ProgramaNome.java
+++ src/ProgramaNome.java
@@ -1,3 +1,4 @@
+//Como queremos utilizar o scanner para ler os dados que o usuário vai digitar e ele não é carregado por padrão, precisamos IMPORTAR o Scanner para que nosso 
 import java.util.Scanner;
 
 //Esse programa deve receber o nome do usuario e exibir este nome com uma mensagem de boas vindas
@@ -6,9 +7,24 @@ public class ProgramaNome {
 	public static void main(String[] args) {
 		//A linha abaixo cria um leitor para ler o teclado do usuario
 		Scanner leitor = new Scanner(System.in);
+		
+		//A linha abaixo cria uma variável (espaço na RAM) para guardar dados do tipo String(texto)
+		String nome;
+		
 		System.out.println("Por favor, digite seu nome: ");
+		//A linha abaixo pega o texto digitado pelo usuário e guarda na variaável "nome"
+		nome = leitor.next();
+		
+		//Para lermos textos com espaço podemos usar o nextline
+		//nome=leitor.nextLine();
+		
 		//esse codigo lê um texto do teclado do usuario
-		System.out.println("Que legal que seu nome é: " + leitor.next());
+		System.out.println("Que legal que seu nome é: " + nome);
+		
+		System.out.println(nome + ", fico feliz que você esteja usando o meu programa");
+		
+		//Após terminarmos de usar o leitor, precisamos fechar
+		leitor.close();
 	}
 
 }
